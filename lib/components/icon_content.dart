a6b458a7d8fce6439ea8ac7f67d741586b09b18a
haile eshetu <michaelhaile1222@gmail.com>
2024-12-06 00:23:30 +0300

    some file structure and modfication

--- lib/components/icon_content.dart
+++ lib/components/icon_content.dart
@@ -1,28 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:bmi_calculator/constants.dart';
+//import 'package:bmi_calculator/constants.dart';
 
-class IconContent extends StatelessWidget {
-  IconContent({this.icon, this.label});
 
-  final IconData icon;
+const labelStyel=TextStyle(
+            fontSize: 20.0,
+            color: Colors.white,
+          );
+
+
+class IconCard extends StatelessWidget {
   final String label;
+  final IconData icon;
+
+  IconCard({required this.label, required this.icon});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch, // Ensures the column stretches to fill space
       children: <Widget>[
-        Icon(
-          icon,
-          size: 80.0,
-        ),
-        SizedBox(
-          height: 15.0,
-        ),
+        Icon(icon, size: 80.0), // Adjust the icon size as necessary
+        SizedBox(height: 20.0),
         Text(
           label,
-          style: kLabelTextStyle,
-        )
+          style: labelStyel,
+          textAlign: TextAlign.center, // Centers the text
+        ),
       ],
     );
   }
--- lib/components/reusable_card.dart
+++ lib/components/reusable_card.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour, this.cardChild, this.onPress});
-
-  final Color colour;
+  final Color color;
   final Widget cardChild;
-  final Function onPress;
+
+  ReusableCard({required this.color, required this.cardChild});
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onPress,
-      child: Container(
-        child: cardChild,
-        margin: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          color: colour,
-          borderRadius: BorderRadius.circular(10.0),
-        ),
+    return Container(
+      child: cardChild,
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
